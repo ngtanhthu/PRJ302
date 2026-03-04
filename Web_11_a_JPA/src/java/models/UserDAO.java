da4fc7aff491470df1d6f49971648ac4d3d54ee4
bbyhthu1112-ai <bbyhthu1112@gmail.com>
2026-03-04 23:13:33 +0700

    Update

--- Web_11_a_JPA/src/java/models/UserDAO.java
+++ Web_11_a_JPA/src/java/models/UserDAO.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,50 +10,76 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import models.UserDTO;
 import utils.DbUtils;
+import utils.JPAUtil;
 
-/**
- *
- * @author ACER
- */
 public class UserDAO {
-    
-    public UserDAO(){
+
+    public UserDAO() {
     }
+
     public UserDTO searchById(String username) {
+        EntityManager em = JPAUtil.getEntityManager();
         try {
-            Connection conn = DbUtils.getConnection();
-            String sql = "SELECT * FROM tblUsers "
-                    + " WHERE userID=?";
-            System.out.println(sql);
-            PreparedStatement pst = conn.prepareStatement(sql);
-            pst.setString(1, username);
-            ResultSet rs = pst.executeQuery();
-            
-            UserDTO user = null;
-            while (rs.next()) {
-                String userID = rs.getString("userID");
-                String fullName = rs.getString("fullName");
-                String password = rs.getString("password");
-                String roleID = rs.getString("roleID");
-                boolean status = rs.getBoolean("status");
-                user = new UserDTO(userID, fullName, password, roleID, status);
-            }
-            
-            System.out.println(user);
-            
-            return user;
-        } catch (Exception e) {
-            return null;
+            return em.find(UserDTO.class, username);
+        } finally{
+            em.close();
         }
     }
-  
-    public UserDTO login(String username, String password){
-        UserDTO user = searchById(username);
-        if(user!=null && user.getPassword().equals(password)){
-            return user;
-        }else{ 
+
+    public UserDTO login(String username, String password) {
+        UserDTO u = searchById(username);
+        if (u != null && u.getPassword().equals(password)) {
+            return u;
+        }
         return null;
     }
-}
+    
+      public boolean add(UserDTO u) {
+        EntityManager em = JPAUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+
+            // Add
+            em.persist(u);
+            ///////
+
+            tx.commit();
+            return true;
+
+        } catch (Exception e) {
+            tx.rollback();
+            return false;
+        } finally {
+            em.close();
+        }
+    }
+
+    public boolean update(UserDTO u) {
+        EntityManager em = JPAUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+
+            // Update
+            em.merge(u);
+            ///////
+
+            tx.commit();
+            return true;
+
+        } catch (Exception e) {
+            tx.rollback();
+            return false;
+        } finally {
+            em.close();
+        }
+    }
+
 }
--- Web_11_a_JPA/src/java/utils/JPAUtil.java
+++ Web_11_a_JPA/src/java/utils/JPAUtil.java
@@ -4,10 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-/**
- *
- * @author tungi
- */
 public class JPAUtil {
     private static final EntityManagerFactory emf =
             Persistence.createEntityManagerFactory("Web_11_a_JPAPU");
